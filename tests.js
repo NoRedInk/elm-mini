c0cea390e4ed4740188f5b9ca58a641020df1e32
Angela Wang <angelawang@meteor.com>
2016-03-31 12:09:48 -0700

    Implement reducer and unreducer

--- tests.js
+++ tests.js
@@ -1,3 +1,27 @@
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
+
 var tuples =
     [
         { ctor: "_Tuple0" },
@@ -13,6 +37,13 @@ var lists =
         { ctor: '::',
          _0: 2,
          _1: { ctor: '[]' }
+        },
+        { ctor: '::',
+        _0: 1,
+        _1: {ctor: '::',
+            _0: 2,
+            _1: {ctor: '[]'}
+            }
         }
     ];
 
@@ -96,17 +127,82 @@ var main = function(reducer){
     });
 };
 
-
 // This is the function to perform the minification in
 // It takes an object, and should return an object
 // the goal is to preserve the meaning on the object
-var reducer = function(object){
-    return object;
+var reducer = function(data){
+    var object = Object.assign({}, data);
+    if (object['ctor']) {
+      if ((object['ctor']).charAt(0) === '_') {
+        // tuple
+        delete object['ctor'];
+        var data = [];
+        Object.keys(object).forEach(function(key) {
+          var i = parseInt(key.slice(1));
+          data[i] = object[key];
+        });
+        var reduced = {};
+        reduced["_t"] = data;
+        return reduced;
+      } else {
+        // list
+        var list = [];
+        var item = object;
+        while (item['ctor'] !== '[]') {
+          list.push(item['_0']);
+          item = item['_1'];
+        }
+        return list;
+      }
+    } else {
+      // record
+      return object;
+    }
 };
 
 // inverse of reducer
+// tuple: {_t: []}, list: [], record: {}
 var unreducer = function(object){
-    return object;
+    if (Array.isArray(object)) {
+      // list
+      var list = {};
+      var current = list;
+      var i = 0;
+      while (i < object.length) {
+        current['ctor'] = "::";
+        current['_0'] = object[i];
+        current['_1'] = {};
+        current = current['_1'];
+        i++;
+      }
+      current['ctor'] = "[]";
+      return list;
+    } else {
+      var keys = Object.keys(object);
+      if (keys.length === 1) {
+        if (keys[0] === "_t") {
+          // tuple
+          var tuple = {};
+          var ctor = '_Tuple' + keys.length;
+          tuple['ctor'] = ctor;
+          object['_t'].forEach(function(item, i) {
+            var key = '_' + i;
+            tuple[key] = item;
+          });
+          return tuple;
+        } else {
+          // record
+          return object;
+        }
+      } else {
+        // record
+        return object;
+      }
+    }
+
 };
 
 main(reducer);
+// lists.forEach(function(list) {
+//   console.log(unreducer(reducer(list)));
+// });
